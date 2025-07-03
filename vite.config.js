0f9e59726f2c2cd1bfa6d77beb126c5fc300dba2
camposeffect <geral@vallismusic.com>
2025-07-03 19:00:31 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',  // Certifique-se de que a pasta de saída é "dist"
+    rollupOptions: {
+      input: {
+        main: './index.html', // Certifique-se de que o entry point é o seu index.html
+      },
+    },
+  },
 })
